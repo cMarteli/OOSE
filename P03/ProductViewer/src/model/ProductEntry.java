eef5c09bc44b287eb02b458a748eec1630489ddc
C <19598552@student.curtin.edu.au>
2022-03-27 18:08:18 +0800

    input file not reading, added abstract class

---
+++ P03/ProductViewer/src/model/ProductEntry.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.Objects;
+
+/**
+ * interface
+ * Represents any entry. Entries have names.
+ */
+public interface ProductEntry
+{
+    public String getName();
+}
